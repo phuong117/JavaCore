a82a9819d25ec9d0f42753e2b3e565cfca0a4ef6
phuong117 <nguyenminhphuong11072002@gmail.com>
2023-02-22 00:18:28 +0700

    them java_03

---
+++ src/Java_03/Hello.java
@@ -0,0 +1,11 @@
+package Java_03;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // sout => viet nhanh lenh print
+        System.out.println("Xin Chao");
+    }
+
+
+}
--- src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("phuong an lon va an cut");
-    }
-}
